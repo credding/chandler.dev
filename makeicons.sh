f10a898bcb6173f5f6e841f5e7b09c2a639bcc9d
Chandler Redding <c@chandler.dev>
2023-11-21 19:48:38 -0700

    tweak icon sizes

--- makeicons.sh
+++ makeicons.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
 inkscape \
+  --export-filename='favicon-16x16.png' \
+  --export-width='16' \
+  --export-height='16' \
+  'favicon.svg'
+
+inkscape \
+  --export-filename='favicon-32x32.png' \
+  --export-width='32' \
+  --export-height='32' \
+  'favicon.svg'
+
+inkscape \
   --export-filename='favicon-48x48.png' \
   --export-width='48' \
   --export-height='48' \
   'favicon.svg'
 
 convert \
+  'favicon-16x16.png' \
+  'favicon-32x32.png' \
   'favicon-48x48.png' \
   'favicon.ico'
 
